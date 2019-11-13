1ea6f2a34c0c2747f9649646fd254b57aff19687
Lindsay <lindsay.lbm@gmail.com>
2019-11-13 16:05:54 -0600

    Updates

---
+++ precache-manifest.e3e498e080125392b8a1032e3c1c33d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f2896a437fd78851bc4dd8a19693772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "220fe3f5286f9f776fa1",
+    "url": "/static/css/main.484f6a4c.chunk.css"
+  },
+  {
+    "revision": "5e9d43a13e29d11f4a25",
+    "url": "/static/js/2.eb47af43.chunk.js"
+  },
+  {
+    "revision": "220fe3f5286f9f776fa1",
+    "url": "/static/js/main.0dcb155f.chunk.js"
+  },
+  {
+    "revision": "93178cac179f84ce7888",
+    "url": "/static/js/runtime-main.15eb2435.js"
+  }
+]);
